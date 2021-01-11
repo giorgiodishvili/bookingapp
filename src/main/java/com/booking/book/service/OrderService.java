d6537a26ccf97671ee89fefc677d4769104d8e4b
mikheil Tchelidze <chelomisha@gmail.com>
2021-01-11 23:01:41 +0400

    code refactor

--- src/main/java/com/booking/book/service/OrderService.java
+++ src/main/java/com/booking/book/service/OrderService.java
@@ -1,20 +1,50 @@
 package com.booking.book.service;
 
+import com.booking.book.dao.OrderRepository;
 import com.booking.book.entity.Order;
+import com.booking.book.exception.OrderNotFoundException;
+import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
-public interface OrderService {
+@Service
+public class OrderService {
 
-    List<Order> getAllOrders();
+    private final OrderRepository orderRepository;
 
-    Order getOrderById(Long id);
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
 
-    void saveOrder(Order order);
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
 
-    void deleteById(Long theId);
+    public Order getOrderById(Long id) {
+        return orderRepository.findById(id).orElseThrow(OrderNotFoundException::new);
+    }
 
-    void removeOrder(Long id);
+    public void saveOrder(Order order) {
 
+        List<Order> orders = orderRepository.isTimeAvailableByRoomId(order.getRoomId(), order.getStartDateTime(), order.getEndDateTime());
 
+        int difference = order.getStartDateTime().compareTo(LocalDate.now());
+        boolean moreThanCurrentDate = difference >= 0;
+        if (orders.size() == 0 && moreThanCurrentDate) {
+
+            Order save = orderRepository.save(order);
+        } else {
+
+            throw new RuntimeException("Cant be added");
+        }
+    }
+
+    public void deleteById(Long theId) {
+        orderRepository.deleteById(theId);
+    }
+
+    public void removeOrder(Long id) {
+        orderRepository.deleteById(id);
+    }
 }
